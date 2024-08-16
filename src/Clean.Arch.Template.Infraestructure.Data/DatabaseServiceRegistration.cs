565ac2c79e7e9298178e5aae1015420056458aab
andre <andre.augusto222@gmail.com>
2024-08-16 05:09:50 -0300

    Add project files.

---
+++ src/Clean.Arch.Template.Infraestructure.Data/DatabaseServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Clean.Arch.Template.Infraestructure.Data;
+public static class DatabaseServiceRegistration
+{
+    public static void AddDatabaseServices(this IServiceCollection services, DatabaseOptions configuration)
+    {
+        var connectionString = configuration.ConnectionString;
+#if SqlServer
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+#elif PostgreSQL
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+#elif MySQL
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+#elif SQLite
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(connectionString));
+#else
+        throw new InvalidOperationException("No valid database provider selected.");
+#endif
+    }
+}
---
+++ src/Clean.Arch.Template.WebAPI/Program.cs
@@ -0,0 +1,28 @@
+using Clean.Arch.Template.Infraestructure.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDatabaseServices(builder.Configuration.GetSection("DatabaseOptions").Get<DatabaseOptions>()!);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
